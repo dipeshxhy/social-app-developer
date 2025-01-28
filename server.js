c6e10c857f0bdb520ed9498946b812b5549285d3
Dipesh Chaudhary <158567129+dipeshxhy@users.noreply.github.com>
2025-01-28 13:02:52 +0545

    db connected

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app=express();
+
+connectDB()
+
 app.use(express.json());
 
 const PORT=process.env.PORT || 5000;
